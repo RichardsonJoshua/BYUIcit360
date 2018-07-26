9b20db26ec39a6bc4cbd1989c840346c467a11ff
Joshua Richardson <35509200+RichardsonJoshua@users.noreply.github.com>
2018-07-26 14:05:44 -0500

    Create appRunner.java
    MVC

---
+++ appRunner.java
@@ -0,0 +1,14 @@
+package sample.controller;
+
+
+//this is the runner for the app
+public class appRunner {
+
+	public static void main(String[] args) {
+		appController baseApp = new appController();
+		
+		baseApp.start();
+
+	}
+
+}
