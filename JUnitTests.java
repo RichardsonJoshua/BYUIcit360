ded63e3035ac3c31c9a849f8418d791352958e68
Joshua Richardson <35509200+RichardsonJoshua@users.noreply.github.com>
2018-07-26 06:58:15 -0500

    Update JUnitTests.java

--- JUnitTests.java
+++ JUnitTests.java
@@ -6,22 +6,24 @@ import org.junit.Test;
 
 public class JUnit {
 
-	
+	//no main method in JUnit
+	//It's for testing only
 	 
+	//Test multiplication method of the SimpleCalc class
 	   @Test
 	   public void test_multiplication() {
 	      System.out.println("Multiplication Test") ;
 	      SimpleCalc newMultiply = new SimpleCalc(100,3) ;
 	      assertTrue(newMultiply.multiplication() == 300) ;
 	   }
-	   
+	   //Test addition method of the SimpleCalc class
 	   @Test
 	   public void test_addition() {
 	      System.out.println("Testing addition...") ;
 	      SimpleCalc newAddition = new SimpleCalc(100,3) ;
 	      assertTrue(newAddition.addition() == 103) ;
 	   }
-	   
+	   //Test subtraction method of the SimpleCalc class
 	   @Test
 	   public void test_subtraction() {
 	      System.out.println("Testing subtraction...") ;
