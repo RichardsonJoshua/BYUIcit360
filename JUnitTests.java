7de9d39c090b1e8a5d56265f5c1746d91aa3ea6a
Joshua Richardson <35509200+RichardsonJoshua@users.noreply.github.com>
2018-07-26 07:16:08 -0500

    Update JUnitTests.java

--- JUnitTests.java
+++ JUnitTests.java
@@ -10,25 +10,33 @@ public class JUnit {
 	//It's for testing only
 	 
 	//Test multiplication method of the SimpleCalc class
-	   @Test
+	  @Test
 	   public void test_multiplication() {
 	      System.out.println("Multiplication Test") ;
 	      SimpleCalc newMultiply = new SimpleCalc(100,3) ;
 	      assertTrue(newMultiply.multiplication() == 300) ;
 	   }
 	   //Test addition method of the SimpleCalc class
-	   @Test
+	  @Test
 	   public void test_addition() {
-	      System.out.println("Testing addition...") ;
+	      System.out.println("Addition Test") ;
 	      SimpleCalc newAddition = new SimpleCalc(100,3) ;
 	      assertTrue(newAddition.addition() == 103) ;
 	   }
 	   //Test subtraction method of the SimpleCalc class
-	   @Test
+	  @Test
 	   public void test_subtraction() {
-	      System.out.println("Testing subtraction...") ;
+	      System.out.println("Subtraction Test") ;
 	      SimpleCalc newSubtraction = new SimpleCalc(100,3) ;
 	      assertTrue(newSubtraction.subtraction() == 97) ;
+	   }
+	
+	//Test division method of the SimpleCalc class
+	 @Test
+	   public void test_division() {
+	      System.out.println("Division Test") ;
+	      SimpleCalc newDivision = new SimpleCalc(100,2) ;
+	      assertTrue(newDivision.division() == 50) ;
 	   }
 	   
 }
